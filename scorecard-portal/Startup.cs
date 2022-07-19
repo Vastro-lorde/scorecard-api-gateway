70ba0bf61f2dac54a70dd91408da9f8ac91a804d
Vastro-lorde <omatsolaseund@outlook.com>
2022-07-19 20:56:49 +0100

    Resolving CORS

--- scorecard-portal/Startup.cs
+++ scorecard-portal/Startup.cs
@@ -31,6 +31,7 @@ namespace scorecard_portal
             services.AddOcelot();
             services.AddControllers();
             services.AddSwaggerForOcelot(Configuration);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +50,14 @@ namespace scorecard_portal
             });
 
             app.UseRouting();
+
+            app.UseCors(builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
 
             app.UseAuthorization();
 
